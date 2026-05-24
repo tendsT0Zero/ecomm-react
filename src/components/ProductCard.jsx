58e1bdae672d7a1a05055a6af5d78e0bd04e1f2e
tendsT0Zero <nayeemofficial80@gmail.com>
2026-05-24 15:51:27 +0600

    Basic Login Page done

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useCart } from "../context/CartContext";
+
+function ProductCard({product}) {
+    const { addToCart } =useCart();
+  return (
+    <div className="bg-white p-4 rounded-xl shadow hover:shadow-lg glex flex-col justify-between">
+      <img
+        src={product.image}
+        alt={product.title}
+        className="h-40 w-full object-contain mb-4"
+      />
+      <div>
+        <h3 className="font-semibold text-gray-800 truncate">
+          {product.title}
+        </h3>
+        <p className="text-blue-600 font-bold">${product.price}</p>
+      </div>
+      <button
+        onClick={() => addToCart(product)}
+        className="mt-4 bg-blue-600 text-white py-2 rounded w-full hover:bg-blue-700"
+      >
+        Add to Cart
+      </button>
+    </div>
+  );
+}
+
+export default ProductCard;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const [name, setName] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (name.trim()) {
+      login(name);
+      navigate("/");
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="flex h-screen justify-center items-center bg-gray-100">
+      <form
+        onSubmit={handleLogin}
+        className="bg-white p-8 rounded shadow-md w-96"
+      >
+        <h2 className="text-2xl font-bold mb-4">Login</h2>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          className="w-full border p-2 mb-4 rounded"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button className="w-full bg-blue-600 text-white py-2 rounded">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
