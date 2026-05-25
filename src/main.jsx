afdfb16be25d05a6e9fbb77f4a0ab4988be2ce24
tendsT0Zero <nayeemofficial80@gmail.com>
2026-05-25 10:43:46 +0600

    wraping app with redux store and tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,23 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import CartProvider from "./context/CartContext.jsx";
-import AuthProvider from "./context/AuthContext.jsx";
+// import CartProvider from "./context/CartContext.jsx";
+// import AuthProvider from "./context/AuthContext.jsx";
 import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <CartProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
           <App />
-        </CartProvider>
-      </AuthProvider>
-    </BrowserRouter>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>,
 );
